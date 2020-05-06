840df371316b395a0567e14656570ced5b27a9ea
sergio9929 <sergio9929@hotmail.com>
2020-05-06 02:31:56 +0200

    soporte para ios

--- jsFunctions.js
+++ jsFunctions.js
@@ -1,9 +1,10 @@
 var suavizado = 480;
 document.addEventListener("DOMContentLoaded", function (event) {
     document.addEventListener("scroll", videoplay);
-    document.getElementById("video").onloadeddata = () => {
+    document.getElementById("video").onloadeddata= () => {
         document.getElementById("altura").setAttribute("style", "height:" + document.getElementById("video").duration * suavizado + "px;");
     };
+    document.getElementById("video").load();
 })
 
 function videoplay() {
